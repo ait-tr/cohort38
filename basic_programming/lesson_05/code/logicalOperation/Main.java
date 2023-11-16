57ffdd7047f0198c732a2a2f7346324acc07b687
azart <al.az.artuhov@gmail.com>
2023-11-16 02:54:06 +0200

    updates lesson 5. class main in logic operation

--- basic_programming/lesson_05/code/logicalOperation/Main.java
+++ basic_programming/lesson_05/code/logicalOperation/Main.java
@@ -3,11 +3,24 @@ package logicalOperation;
 public class Main {
     public static void main(String[] args) {
 //
-        boolean b1 =  ((5 > 7) ^ (true ^ !true) || ((true ^ !false) ^ !true)) && !((5 > 7) && (7 < 9)) || (((2 < 3) || (1 > 0)) && (24 >= 19));
-//                      false          true             false          false
-//                              true                               false
+        boolean b0 =  ((5 > 7) ^ (true ^ !true) || ((true ^ !false) ^ !true)) && !((5 > 7) && (7 < 9)) || (((2 < 3) || (1 > 0)) && (24 >= 19));
+//                      false          true     ||        false         false       false        true         true       true
+//                            true              ||                false                   true                     true               true
+//                                              true                          &&          true          ||                      true
+//                                                                            true                      ||                      true
+//                                                                                                     true
+//        System.out.println(b1);
+        boolean b = (4 > 3)
+                    &&
+                    (5 < 7)
+                    ||
+                    (true ^ false);
 
-
+        boolean b1 =  ((5 > 7) ^ (true ^ !true) || ((true ^ !false) ^ !true))
+                      &&
+                      !((5 > 7) && (7 < 9))
+                      ||
+                      (((2 < 3) || (1 > 0)) && (24 >= 19));
 
         boolean b2 =  ((5 >= 7) && !(true ^ !true) ^ !(!(true || !false) ^ !true)) || !((5 < 7) && !(7 < 9)) || ((!(2 < 3) ^ (1 > 0)) && !(24 >= 19));
         boolean b3 =  ((5 > 7) ^ !(true ^ true) && (!!(true ^ !false) || !true)) || (!(5 > 7) && !(7 > 9)) && ((!(2 < 3) ^ (1 < 0)) || (24 >= 19));
@@ -28,16 +41,16 @@ public class Main {
 //                (!true || !false) && (5 < 7)
 //        );
 
-        System.out.println(
-                (true ^ false) ^ (5 < 7)
-        );
-        System.out.println(
-                ((5 > 7) ^ (true ^ !true) || ((true ^ !false) ^ !true)) && !((5 > 7) && (7 < 9)) || (((2 < 3) || (1 > 0)) && (24 >= 19))
-//                false         true
-//                true         true
-//                true         true
-//                true         true
-        );
+//        System.out.println(
+//                (true ^ false) ^ (5 < 7)
+//        );
+//        System.out.println(
+//                ((5 > 7) ^ (true ^ !true) || ((true ^ !false) ^ !true)) && !((5 > 7) && (7 < 9)) || (((2 < 3) || (1 > 0)) && (24 >= 19))
+////                false         true
+////                true         true
+////                true         true
+////                true         true
+//        );
 
 
     }
