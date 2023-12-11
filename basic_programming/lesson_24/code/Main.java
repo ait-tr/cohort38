4322fceb22612e36cc36bb7982e6ece75b4ad284
Hayk <hayk.inanc@gmail.com>
2023-12-12 00:00:13 +0400

    lesson 24 (git fix)

--- basic_programming/lesson_24/code/Main.java
+++
@@ -1,8 +0,0 @@
-package inheritance_2;
-
-public class Main {
-    public static void main(String[] args) {
-        Dog dog = new Dog("sharik", "labrador", 30);
-        System.out.println(dog);
-    }
-}
